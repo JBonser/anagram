66e68b79a94835c2359fef4c9cff4f9152131e82
jordan bonser <jbonser@uk.ibm.com>
2018-09-03 18:44:01 +0100

    Passed double letter example test

--- anagram.py
+++ anagram.py
@@ -1,2 +1,7 @@
 def find_all(word):
-    return ["a"]
+    anagrams = []
+    for index, char in enumerate(word):
+        word_without_char = word[:index] + word[index+1:]
+        new_word = char + word_without_char
+        anagrams.append(new_word)
+    return anagrams

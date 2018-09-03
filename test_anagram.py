667afc3f8c3ff349cae61564cc117cd5990cc42c
jordan bonser <jbonser@uk.ibm.com>
2018-09-03 17:15:30 +0100

    Added failing two character case

--- test_anagram.py
+++ test_anagram.py
@@ -4,8 +4,11 @@ import unittest
 
 class TestAnagram(unittest.TestCase):
     def test_anagram_single_character(self):
-        result = ["a"]
-        self.assertEqual(result, anagram.find_all("a"))
+        expected_result = ["a"]
+        self.assertEqual(expected_result, anagram.find_all("a"))
 
-    # def test_anagram_two_different_characters(self):
-    #     self.assertEqual("ab", anagram.find_all("ab"))
+    def test_anagram_two_different_characters(self):
+        result = anagram.find_all("ab")
+        self.assertEqual(2, len(result))
+        self.assertTrue("ab" in result)
+        self.assertTrue("ba" in result)

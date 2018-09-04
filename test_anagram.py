1cca95937b7a7f3f47b559df8ac37da35d8ba7e1
jordan bonser <jbonser@uk.ibm.com>
2018-09-04 19:34:03 +0100

    add simple recurse function failing test

--- test_anagram.py
+++ test_anagram.py
@@ -13,15 +13,13 @@ class TestAnagram(unittest.TestCase):
         self.assertTrue("ab" in result)
         self.assertTrue("ba" in result)
 
-    def test_anagram_three_different_characters(self):
-        result = anagram.find_all("abc")
-        self.assertEqual(6, len(result))
-        expected_results = [
-        "abc",
-        "acb",
-        "bac",
-        "bca",
-        "cab",
-        "cba"
-        ]
-        [self.assertTrue(word in result) for word in expected_results]
+    # def test_anagram_three_different_characters(self):
+    #     result = anagram.find_all("abc")
+    #     self.assertEqual(6, len(result))
+    #     expected_results = [ "abc", "acb", "bac", "bca", "cab", "cba"]
+    #     [self.assertTrue(word in result) for word in expected_results]
+
+    def test_recurse_two_different_characters(self):
+        result = anagram.recurse_append_anagrams("a", "b")
+        self.assertEqual(1, len(result))
+        self.assertTrue("ab" in result)
